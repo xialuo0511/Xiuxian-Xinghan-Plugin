564c3647704874d6e0f47797c6ae16e9753176aa
屑洛 <11485889+xialuo03@user.noreply.gitee.com>
2023-03-04 09:24:11 +0800

    调整兑换码

--- apps/User/UserHome.js
+++ apps/User/UserHome.js
@@ -126,7 +126,7 @@ export class UserHome extends plugin {
             }
         }
         //特殊兑换码调整
-        if (data.duihuan[i].name == "测试兑换码") {
+        if (data.duihuan[i].name == "1.3版本参与测试") {
             for (var o = 0; o < data.duihuan[i].qq.length; o++) {
                 if (usr_qq == data.duihuan[i].qq[o].name) {
                     action.push(name);
@@ -136,11 +136,11 @@ export class UserHome extends plugin {
                         await Add_najie_thing(usr_qq, data.duihuan[i].thing[k].name, data.duihuan[i].thing[k].class, data.duihuan[i].thing[k].数量);
                         msg.push("\n" + data.duihuan[i].thing[k].name + "x" + data.duihuan[i].thing[k].数量);
                     }
-                    e.reply("恭喜获得:" + msg);
+                    e.reply("感谢您参与测试！恭喜获得:" + msg);
                     return;
                 }
             }
-            e.reply("test" + data.duihuan[i].qq[1].name);
+            e.reply("您不是测试服成员，无法使用此兑换码");
             return;
         }
         //普通兑换流程
