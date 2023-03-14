df3578f4153cd080891d72709965b6b8542b8911
root <root@localhost>
2023-03-14 18:50:02 +0800

    2023-03-14 18:49:11

--- apps/SecretPlace/SecretPlace.js
+++ apps/SecretPlace/SecretPlace.js
@@ -637,7 +637,7 @@ export class SecretPlace extends plugin {
         let quantity = code[1];
         //获取活动商店数据
         let commodities_list = data.huodongshop_list;
-        commodities_list = commodities_list.filter(name => wupin);
+        commodities_list = commodities_list.filter(name => thing_name);
         //搜索纳戒物品
         let shu = await exist_najie_thing(usr_qq, thing_name, commodities_list.daibi);
         //转为整数
