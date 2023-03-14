54bd2971e288c04245c8c50e75c2c9f9eca593c5
root <root@localhost>
2023-03-14 18:46:07 +0800

    2023-03-14 18:45:21

--- apps/SecretPlace/SecretPlace.js
+++ apps/SecretPlace/SecretPlace.js
@@ -629,8 +629,7 @@ export class SecretPlace extends plugin {
         await Go(e);
 
         //获取输入信息
-        let msg = e.msg.replace(reg, '');
-        msg = msg.replace("#", '');
+        let msg = e.msg.replace("#代币兑换", '');
         //分割文本变数组
         let code = msg.split("\*");
         //获取物品名和数量
