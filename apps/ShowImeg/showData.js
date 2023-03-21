6fd0cd19012ff893f87db50adae83fcea5176cfe
root <root@localhost>
2023-03-21 18:11:49 +0800

    2023-03-21 18:10:45

--- apps/ShowImeg/showData.js
+++ apps/ShowImeg/showData.js
@@ -1370,6 +1370,7 @@ export async function get_najie_img(e) {
     let strand_hp = Strand(player.当前血量, player.血量上限)
     let strand_lingshi = Strand(najie.灵石, najie.灵石上限)
     let pifu = redis.get("xiuxian:player:" + usr_qq + ":lianqipifu")
+    e.reply(pifu);
     if (pifu === 'null') {
         pifu = 0
     }
