54006840eac4579d23936a68e4d723d3cfe8bb07
屑洛 <11485889+xialuo03@user.noreply.gitee.com>
2023-03-27 22:16:24 +0800

    test

--- apps/xunbao/GuessLanternRiddles.js
+++ apps/xunbao/GuessLanternRiddles.js
@@ -201,8 +201,8 @@ export class GuessLanternRiddles extends plugin {
                 await sleep(2000)
                 for (var i = 0; 10 > i; i++) {
                     let obj = this.data.changzhufumoshu_list
-                    let tianluoRandom = Math.floor(Math.random() * (a.length));
-                    let b = a[tianluoRandom].name
+                    let tianluoRandom = Math.floor(Math.random() * (obj.length));
+                    let b = obj[tianluoRandom].name
                     e.reply(b)
                     msg.push("一道金光掉落在地上，走近一看是【" + a + "】")
                     await Add_najie_thing(usr_qq, b, '道具', 1)
