ec446b545c7d1f612663c1d9e7d812b2e42b8f1a
屑洛 <11485889+xialuo03@user.noreply.gitee.com>
2023-04-05 11:40:06 +0800

    1

--- apps/User/UserHome.js
+++ apps/User/UserHome.js
@@ -3617,11 +3617,6 @@ export async function Go(e) {
             return;
         }
     }
-    let player = await Read_player(usr_qq);
-    if (player.当前血量 < 200) {
-        e.reply("你都伤成这样了,就不要出去浪了");
-        return;
-    }
     allaction = true;
     return;
 }
