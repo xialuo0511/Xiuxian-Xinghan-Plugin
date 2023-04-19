6859d744db1799bda084153d91b2d97cd05e3cf7
屑洛 <11485889+xialuo03@user.noreply.gitee.com>
2023-04-19 22:43:02 +0800

    1

--- apps/xunbao/GuessLanternRiddles.js
+++ apps/xunbao/GuessLanternRiddles.js
@@ -178,7 +178,6 @@ export class GuessLanternRiddles extends plugin {
                 all.push("【" + data.xianding[tianluoRandom].name + "】")
             }
             await Add_najie_thing(usr_qq, "树苗", "食材", -10)
-            await ForwardMsg(e, msg)
             e.reply("恭喜获得\n" + all)
         }
         if (thing == "附魔台") {
