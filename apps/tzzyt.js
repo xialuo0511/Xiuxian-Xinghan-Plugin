e4b3d01e2248b02c28e0145405fbe0fd257e12f9
屑洛 <11485889+xialuo03@user.noreply.gitee.com>
2023-04-22 16:45:45 +0800

    1

--- apps/tzzyt.js
+++ apps/tzzyt.js
@@ -2,8 +2,8 @@ import plugin from '../../../lib/plugins/plugin.js'
 
 import data from '../model/XiuxianData.js'
 //如需截图必须引入以下两库
-import puppeteer from '../../../../lib/puppeteer/puppeteer.js';
-import Show from '../../model/show.js';
+import puppeteer from '../../../lib/puppeteer/puppeteer.js';
+import Show from '../model/show.js';
 
 export class tzzyt extends plugin {
     constructor() {
