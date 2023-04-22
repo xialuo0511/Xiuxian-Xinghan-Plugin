e193774a4efffdb8d0bb19a8d3e23c1e125a9193
屑洛 <11485889+xialuo03@user.noreply.gitee.com>
2023-04-22 21:21:50 +0800

    1

--- apps/Xiuxian/xiuxian.js
+++ apps/Xiuxian/xiuxian.js
@@ -1943,6 +1943,11 @@ export async function Gaodenyuansulun(A_player, B_player, last_att, msg, cnt, Ag
         att *= 2.5
         A_player.当前血量 *= 1.5
     }
+    if (B.武器.name == "湛卢") {
+        msg.push("触发特殊技能，获得50%生命加成，150%攻击加成")
+        B.攻击 *= 2.5
+        B.当前血量 *= 1.5
+    }
 
     //终末嗟叹之诗
     if (equipment.武器.name == "终末嗟叹之诗") {
