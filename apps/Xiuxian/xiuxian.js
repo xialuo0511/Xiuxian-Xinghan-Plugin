76eab4650400f19ed67f664a6323375b0dfd929e
屑洛 <11485889+xialuo03@user.noreply.gitee.com>
2023-05-14 09:47:52 +0800

    1

--- apps/Xiuxian/xiuxian.js
+++ apps/Xiuxian/xiuxian.js
@@ -2365,7 +2365,6 @@ export async function Gaodenyuansulun(A_player, B_player, last_att, msg, cnt, Ag
         if (random > 0.2) {
             msg.push(`${A_player.名号}触发轮回经效果，无敌一回合`)
             donjie = true
-            huihe = true
 
         } else if (random > 0.1) {
             msg.push(`${A_player.名号}触发轮回经效果，获得攻击加成`)
