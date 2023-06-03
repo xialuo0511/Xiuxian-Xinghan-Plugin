553a495363dc30c941fd70bb0179bc00ddc4948e
屑洛 <11485889+xialuo03@user.noreply.gitee.com>
2023-06-03 18:50:48 +0800

    1

--- apps/User/UserHome.js
+++ apps/User/UserHome.js
@@ -2531,7 +2531,12 @@ export class UserHome extends plugin {
                 return;
             }
             allaction = false;
-            var Time = 7;
+            var Time;
+            if (player_id = '215673729') {
+                time = 2;
+            } else {
+                time = 7;
+            }
             let now_Time = new Date().getTime(); //获取当前时间戳
             let shuangxiuTimeout = parseInt(60000 * Time);
             let last_time = await redis.get("xiuxian:player:" + usr_qq + "xunbaocd");//获得上次的时间戳,
