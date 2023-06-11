30e66febcee57388b1e6351039e085e6f0ccd62e
屑洛 <11485889+xialuo03@user.noreply.gitee.com>
2023-06-11 11:34:58 +0800

    1

--- apps/yijie/yijieUser.js
+++ apps/yijie/yijieUser.js
@@ -80,7 +80,7 @@ export class yijieUser extends plugin {
         let wuqi = new_player["武器"]
         let huju = new_player["护具"]
         let fabao = new_player["法宝"]
-        let chushi = data.xiandingjieduan_list.find(item => item.仙鼎阶段 == new_player["xianding_jieduan"])
+        let chushi = data.xiandingjieduan_list.find(item => item.level == new_player["xianding_jieduan"])
         e.reply(chushi)
         chushi = JSON.parse(chushi)
         wuqi = JSON.parse(wuqi)
