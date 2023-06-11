01dd43e073766579c7a776385521ff9310de1907
屑洛 <11485889+xialuo03@user.noreply.gitee.com>
2023-06-11 16:10:05 +0800

    1

--- apps/yijie/yijieUser.js
+++ apps/yijie/yijieUser.js
@@ -142,6 +142,7 @@ export class yijieUser extends plugin {
         if (player) {
             return;
         }
+        player = JSON.parse(...player)
         e.reply(player.血量上限)
         //         e.reply(`〓异界基础面板〓
         // 血量上限：${Number(player.血量上限)}
