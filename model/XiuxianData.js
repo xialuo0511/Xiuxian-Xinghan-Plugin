af1dbdc3f52867735d6a55158cd5d8d7459553a2
屑洛 <11485889+xialuo03@user.noreply.gitee.com>
2023-06-17 22:58:52 +0800

    1

--- model/XiuxianData.js
+++ model/XiuxianData.js
@@ -89,7 +89,7 @@ class XiuxianData {
         //仙鼎阶段
         this.xiandingjieduan_list = JSON.parse(fs.readFileSync(`${this.lib_path}/异界/仙鼎阶段.json`));
         //异界秘境
-        this.yijie_mijing = JSON.parse(fs.readFileSync(`${this.lib_path}/异界/秘境/仙鼎阶段.json`));
+        this.yijie_mijing = JSON.parse(fs.readFileSync(`${this.lib_path}/异界/秘境/秘境列表.json`));
 
 
         //加载丹药列表
