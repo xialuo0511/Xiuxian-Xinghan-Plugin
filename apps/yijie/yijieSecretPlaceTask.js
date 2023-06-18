860d5c1e14cbe0884636b1df117af40692da5782
屑洛 <11485889+xialuo03@user.noreply.gitee.com>
2023-06-18 15:37:03 +0800

    1

--- apps/yijie/yijieSecretPlaceTask.js
+++ apps/yijie/yijieSecretPlaceTask.js
@@ -90,7 +90,7 @@ export class yijieSecretPlaceTask extends plugin {
 
 
             let B_player = await yijie_zhanlijisuan(monster)
-            let A_win = `${player.名号}击败了${monster.名号}`;
+            let A_win = `击败了${monster.名号}`;
             let B_win = `${monster.名号}击败了${player.名号}`;
             var thing_name;
             var thing_class;
