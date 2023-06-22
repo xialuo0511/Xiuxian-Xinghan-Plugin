55238ad3237612dc18a96da321a3cd3799f647e4
屑洛 <11485889+xialuo03@user.noreply.gitee.com>
2023-06-22 23:06:52 +0800

    1

--- apps/yijie/xianding.js
+++ apps/yijie/xianding.js
@@ -100,12 +100,11 @@ export class xianding extends plugin {
                 xiuwei = xiuwei + l.出售价 * quantity;
             }
         }
-        await Add_修为(usr_qq, xiuwei);
         if (xiuwei == 0) {
             e.reply("您的背包里没有一本仙鼎遗书，本次炼化提高的经验为0")
             return;
         } else {
-            e.reply(`一键炼化成功！本次炼化共提升仙鼎经验*${expshuliang}`)
+            e.reply(`一键炼化成功！本次炼化共提升仙鼎经验*${xiuwei}`)
             return;
         }
     }
