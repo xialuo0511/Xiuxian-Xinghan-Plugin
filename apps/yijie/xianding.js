52289616cb829ee6b679bcb1f49ab41a4df25fb2
root <root@localhost>
2023-06-22 12:20:43 +0800

    2023-06-22 12:20:09

--- apps/yijie/xianding.js
+++ apps/yijie/xianding.js
@@ -102,7 +102,9 @@ export class xianding extends plugin {
             if (shuliang > 0) {
                 let thing = beibao.道具.find(item => item.name == i);
                 await Add_xianding_exp(usr_qq, thing.出售价 * shuliang)
-                await Add_yijie_beibao_thing(usr_qq, i, "道具", (0-shuliang))
+                let lingshi = 0 - shuliang
+                e.reply(lingshi)
+                await Add_yijie_beibao_thing(usr_qq, i, "道具", lingshi)
                 expshuliang = expshuliang + thing.出售价 * shuliang
                 if (i.includes("一")) {
                     yishu1 = shuliang
