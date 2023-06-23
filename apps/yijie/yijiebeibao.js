2d10942ac1ceda95cd37fbfff11af83ec7ca9f06
屑洛 <11485889+xialuo03@user.noreply.gitee.com>
2023-06-23 21:00:07 +0800

    1

--- apps/yijie/yijiebeibao.js
+++ apps/yijie/yijiebeibao.js
@@ -115,7 +115,7 @@ export class yijiebeibao extends plugin {
             cishu = 0
         }
 
-        lishi.push(...a)
+        lishi.push(a.toString())
         await redis.set("xiuxian:box:player:" + usr_qq + ":" + thing.id + "log", lishi.toString())
         await redis.set("xiuxian:box:player:" + usr_qq + ":" + thing.id, cishu)
         return;
