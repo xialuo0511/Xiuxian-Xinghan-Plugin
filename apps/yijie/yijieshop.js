bc7a17ba2eab63615eb4c01b2e1232ddbab4d107
屑洛 <11485889+xialuo03@user.noreply.gitee.com>
2023-06-23 23:08:37 +0800

    1

--- apps/yijie/yijieshop.js
+++ apps/yijie/yijieshop.js
@@ -132,7 +132,7 @@ export class yijieshop extends plugin {
 export async function Go(e) {
     let usr_qq = e.user_id;
     //有无存档
-    let ifexistplay = await existplayer(usr_qq);
+    let ifexistplay = await yijie_existplayer(usr_qq);
     if (!ifexistplay) {
         return;
     }
