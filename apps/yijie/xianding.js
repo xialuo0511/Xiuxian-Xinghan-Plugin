72030bd06b7d49ead884f19d3fa4c6e5d02ef3bb
屑洛 <11485889+xialuo03@user.noreply.gitee.com>
2023-06-23 08:42:41 +0800

    1

--- apps/yijie/xianding.js
+++ apps/yijie/xianding.js
@@ -73,7 +73,7 @@ export class xianding extends plugin {
         player["防御"] += chushi["初始防御"]
         player["血量上限"] += chushi["初始生命"]
         player["xianding_exp"] = new_exp
-        await Write_yijie_player(usr_qq, new_player);
+        await Write_yijie_player(usr_qq, player);
         e.reply(`突破成功！仙鼎升到了${player.xianding_level + 1}级，为你提供的力量提高了！可前往【我的面板】查看`)
         return;
     }
