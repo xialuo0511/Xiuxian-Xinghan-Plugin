4faa2a941fc02311848ec350309dd7c5d19124d6
屑洛 <11485889+xialuo03@user.noreply.gitee.com>
2023-06-27 12:03:33 +0800

    1

--- apps/ShowImeg/showData.js
+++ apps/ShowImeg/showData.js
@@ -1257,7 +1257,7 @@ export async function get_yijie_player_img(e) {
     let fabao = player.法宝
     let taozhuang = "暂无套装效果"
     let find_tz = data.yijie_taozhuang.find(item => item.wuqi == wuqi.name && item.huju == huju.name && item.fabao == fabao.name).context;
-    if (!find_tz) {
+    if (find_tz) {
         taozhuang = find_tz
     }
     let player_data = {
