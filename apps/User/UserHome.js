fcfbbccd125e5826fb6561d45eab475430fe56f9
屑洛 <11485889+xialuo03@user.noreply.gitee.com>
2023-06-28 13:31:30 +0800

    1

--- apps/User/UserHome.js
+++ apps/User/UserHome.js
@@ -2532,7 +2532,7 @@ export class UserHome extends plugin {
             }
             allaction = false;
             var Time = 0;
-            if (usr_qq == "215673729" || usr_qq == "1204963735") {
+            if (usr_qq == "3969712084617166329" || usr_qq == "17143787007936634733") {
                 Time = 2;
             } else {
                 Time = 7;
@@ -2544,7 +2544,7 @@ export class UserHome extends plugin {
             if (now_Time < last_time + shuangxiuTimeout) {
                 let Couple_m = Math.trunc((last_time + shuangxiuTimeout - now_Time) / 60 / 1000);
                 let Couple_s = Math.trunc(((last_time + shuangxiuTimeout - now_Time) % 60000) / 1000);
-                if (usr_qq == "215673729" || usr_qq == "1204963735") {
+                if (usr_qq == "3969712084617166329" || usr_qq == "17143787007936634733") {
                     e.reply("您受到了寻宝赐福，正在归来途中.....\n" + `还需要  ${Couple_m}分 ${Couple_s}秒。`);
                 } else {
                     e.reply("正在归来途中.....\n" + `还需要  ${Couple_m}分 ${Couple_s}秒。`);
