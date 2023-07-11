9cb5fd1efea8e9098b246bbb38e23a8296a44fb3
xialuo03 <2531606029@qq.com>
2023-07-11 22:13:56 +0800

    1

--- apps/yijie/yijieSecretPlaceTask.js
+++ apps/yijie/yijieSecretPlaceTask.js
@@ -131,48 +131,39 @@ export class yijieSecretPlaceTask extends plugin {
                 msg.push(A_win + "\n")
                 msg.push(`在秘境探索的中途，收获了【${thing_name}】`)
               }
-              if (weizhi.name.includes("仙鼎历练")) {
-                await Add_yijie_beibao_thing(player_id, thing_name, thing_class, 1)
-              } else {
-                if (m == "探险者的春天") {
-                  if (random1 <= 0.25) {
-                    msg.push(`本次探索触发了【探险者的春天】效果，收益翻倍！`)
-                    await Add_yijie_beibao_thing(player_id, thing_name, thing_class, 2)
-                  } else {
-                    await Add_yijie_beibao_thing(player_id, thing_name, thing_class, 1)
-                  }
-                } else {
-                  await Add_yijie_beibao_thing(player_id, thing_name, thing_class, 1)
-                }
+              let shu = 1
+              if (!weizhi.name.includes("仙鼎历练") && m == "探险者的春天") {
+                msg.push(`本次探索触发了【探险者的春天】效果，收益翻倍！`)
+                shu = 2
               }
-            }
-            let arr = action;
-            //把状态都关了
-            arr.shutup = 1; //闭关状态
-            arr.working = 1; //降妖状态
-            arr.power_up = 1; //渡劫状态
-            arr.Place_action = 1; //秘境
-            arr.Place_actionplus = 1; //沉迷状态
-            //结束的时间也修改为当前时间
-            arr.end_time = new Date().getTime();
-            //结算完去除group_id
-            delete arr.group_id;
-            //写入redis
-            await redis.set(
-              'xiuxian:yijie:player:' + player_id + ':action',
-              JSON.stringify(arr)
-            );
-            //发送消息
-            if (is_group) {
-              await this.pushInfo(push_address, is_group, msg);
-            } else {
-              await this.pushInfo(player_id, is_group, msg);
+              await Add_yijie_beibao_thing(player_id, thing_name, thing_class, shu)
+              let arr = action;
+              //把状态都关了
+              arr.shutup = 1; //闭关状态
+              arr.working = 1; //降妖状态
+              arr.power_up = 1; //渡劫状态
+              arr.Place_action = 1; //秘境
+              arr.Place_actionplus = 1; //沉迷状态
+              //结束的时间也修改为当前时间
+              arr.end_time = new Date().getTime();
+              //结算完去除group_id
+              delete arr.group_id;
+              //写入redis
+              await redis.set(
+                'xiuxian:yijie:player:' + player_id + ':action',
+                JSON.stringify(arr)
+              );
+              //发送消息
+              if (is_group) {
+                await this.pushInfo(push_address, is_group, msg);
+              } else {
+                await this.pushInfo(player_id, is_group, msg);
+              }
             }
           }
         }
       }
     }
-  }
 
   /**
    * 推送消息，群消息推送群，或者推送私人
@@ -181,14 +172,14 @@ export class yijieSecretPlaceTask extends plugin {
    * @returns {Promise<void>}
    */
   async pushInfo(id, is_group, msg) {
-    if (is_group) {
-      await Bot.pickGroup(id)
-        .sendMsg(msg)
-        .catch(err => {
-          Bot.logger.mark(err);
-        });
-    } else {
-      await common.relpyPrivate(id, msg);
+      if (is_group) {
+        await Bot.pickGroup(id)
+          .sendMsg(msg)
+          .catch(err => {
+            Bot.logger.mark(err);
+          });
+      } else {
+        await common.relpyPrivate(id, msg);
+      }
     }
   }
-}
