a9a398daedbf8aee1b41be24324f422da82bee0c
xialuo03 <2531606029@qq.com>
2023-07-13 10:01:02 +0800

    钻石1

--- apps/yijie/yijieUser.js
+++ apps/yijie/yijieUser.js
@@ -349,12 +349,7 @@ export class yijieUser extends plugin {
             if (mugao > 0) {
                 await Add_yijie_饱食度(usr_qq, -100)
                 await redis.set("xiuxian:yijie:player:" + usr_qq + "xunbaocd", now_Time);
-                if (isNotNull(mugao) && mugao > 0) {
-                    await Add_yijie_beibao_thing(usr_qq, "", "玄蛛网", -1);
-                    mugao = 1
-                } else {
-                    mugao = 0;
-                }
+                await Add_yijie_beibao_thing(usr_qq, "", "玄蛛网", -1);
                 await Add_yijie_beibao_thing(usr_qq, "幽静谷", "道具", -1);
                 if (math > 0.90 && math < 1) {
                     e.reply(`${fanbei}你在【幽静谷】发现了${1000 * beilv}个星魂币，之后迅速跑走了！`)
@@ -392,12 +387,7 @@ export class yijieUser extends plugin {
             if (mugao > 0) {
                 await Add_yijie_饱食度(usr_qq, -100)
                 await redis.set("xiuxian:yijie:player:" + usr_qq + "xunbaocd", now_Time);
-                if (isNotNull(mugao) && mugao > 0) {
-                    await Add_yijie_beibao_thing(usr_qq, "", "铁镐", -1);
-                    mugao = 1
-                } else {
-                    mugao = 0;
-                }
+                await Add_yijie_beibao_thing(usr_qq, "深邃矿洞", "道具", -1);
                 await Add_yijie_beibao_thing(usr_qq, "铁镐", "道具", -1);
                 if (math > 0.8 && math < 1) {
                     e.reply(`${fanbei}你在【深邃矿洞】挖出了【材料*原金矿】*${10 * beilv}`)
