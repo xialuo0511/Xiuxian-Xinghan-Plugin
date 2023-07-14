dffefafa7de355cfebd7a15d54d017183ff5b18b
xialuo03 <2531606029@qq.com>
2023-07-14 20:52:53 +0800

    1

--- apps/yijie/yijieSecretPlace.js
+++ apps/yijie/yijieSecretPlace.js
@@ -69,7 +69,7 @@ export class yijieSecretPlace extends plugin {
             if (a) {
                 let play_guaiwu = guaiwu[i]
                 let zhanli = await yijie_zhanlijisuan(play_guaiwu)
-                e.reply("当前怪物战力为" + zhanli + play_guaiwu.stringify)
+                e.reply("当前怪物战力为" + zhanli + play_guaiwu.stringify(Object))
                 return;
             }
             //}
