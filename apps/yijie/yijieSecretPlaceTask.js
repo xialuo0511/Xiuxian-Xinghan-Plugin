fe60bbf5b9113c33255bd6816961f75bab13c347
xialuo03 <2531606029@qq.com>
2023-07-15 21:23:46 +0800

    1

--- apps/yijie/yijieSecretPlaceTask.js
+++ apps/yijie/yijieSecretPlaceTask.js
@@ -131,7 +131,6 @@ export class yijieSecretPlaceTask extends plugin {
                 rate += contents[i].rate;
                 if (rand < rate) {
                   let item = contents[i].items[Math.floor(Math.random() * contents[i].items.length)];
-                  await Add_yijie_beibao_thing(player_id, item.name, item.class, item.amount);
                   thing_name = item.name;
                   thing_class = item.class;
                   break;
