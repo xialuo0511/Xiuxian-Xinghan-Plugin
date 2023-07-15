90e3a2a0e3b342666c48f4b3ceab327d454c1463
xialuo03 <2531606029@qq.com>
2023-07-15 21:26:02 +0800

    1

--- apps/yijie/yijieSecretPlaceTask.js
+++ apps/yijie/yijieSecretPlaceTask.js
@@ -145,7 +145,7 @@ export class yijieSecretPlaceTask extends plugin {
                 msg.push(A_win + "\n")
                 if (!weizhi.name.includes("仙鼎历练") && m == "探险者的春天" && random1 >= 0.8) {
                   msg.push(`本次探索触发了【探险者的春天】效果，收益翻倍！\n`)
-                  shu = 2
+                  shu *= 2
                 }
                 msg.push(`在秘境探索的中途，收获了【${thing_name}】*${shu}`)
               }
