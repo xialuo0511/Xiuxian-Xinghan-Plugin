c341b9cc825b54eda51e7bdfaa35157191a47b0f
xialuo03 <2531606029@qq.com>
2023-10-01 21:36:49 +0800

    尝试为棋局添加时间间隔

--- apps/Game/SudokuGame.js
+++ apps/Game/SudokuGame.js
@@ -227,7 +227,7 @@ export class SudokuGame extends plugin {
 
         e.reply(`恭喜,棋局完成!`);
         let nowTime = now.getTime(); //获取当前日期的时间戳
-        redis.set("xiuxian:shudu:lasttime", nowTime)
+        await redis.set("xiuxian:shudu:lasttime", nowTime)
         board = null;
         sudokukey = false;
         reward(e)
