1890daba33f601d263de637465e1bc9af9655f67
xialuo03 <2531606029@qq.com>
2023-10-02 12:40:52 +0800

    新增数独游戏配置项

--- apps/Game/SudokuGame.js
+++ apps/Game/SudokuGame.js
@@ -47,6 +47,7 @@ export class SudokuGame extends plugin {
                 },
             ]
         })
+        this.xiuxianConfigData = config.getConfig("xiuxian", "xiuxian");
     }
 
 
@@ -62,7 +63,7 @@ export class SudokuGame extends plugin {
         let nowtime = new Date().getTime();
         let othertime = nowtime - lasttime
         othertime = othertime / 60 / 1000
-        if (othertime < 30) {
+        if (othertime < Number(this.xiuxianConfigData.SudukyGame.time)) {
             e.reply('棋局尚需准备，请稍等片刻');
             return;
         }
