2f628f79ee124cb61fc57e4c45cd1028e78a431b
xialuo03 <2531606029@qq.com>
2023-10-21 22:52:06 +0800

    新增调试模块

---
+++ apps/Gulid/Gulid.js
@@ -0,0 +1,40 @@
+import plugin from '../../../../lib/plugins/plugin.js'
+import data from '../../model/XiuxianData.js'
+import config from "../../model/Config.js"
+import { timestampToTime, shijianc, exist_najie_thing, ForwardMsg, Add_najie_thing } from '../Xiuxian/xiuxian.js'
+
+//如需截图必须引入以下两库
+import puppeteer from '../../../../lib/puppeteer/puppeteer.js';
+import Show from '../../model/show.js';
+/**
+ * 作者：湖中屋
+ */
+/**
+ * 药园模块
+ */
+export class Gulid extends plugin {
+    constructor() {
+        super({
+            /** 功能名称 */
+            name: 'Yunzai_Bot_Gulid',
+            /** 功能描述 */
+            dsc: '频道模块',
+            event: 'message',
+            /** 优先级，数字越小等级越高 */
+            priority: 600,//小功能高一些
+            rule: [
+                {
+                    reg: '^#调试频道$',
+                    fnc: 'test'
+                }
+
+            ]
+        })
+        this.xiuxianConfigData = config.getConfig("xiuxian", "xiuxian");
+    }
+
+    async test(e) {
+        e.reply(e.avatar)
+        return;
+    }
+}
