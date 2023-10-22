6c9aacf8c339f526d6d286ce52a4d2f47522d8c3
xialuo03 <2531606029@qq.com>
2023-10-22 12:53:09 +0800

    调试模块

--- apps/User/UserAction.js
+++ apps/User/UserAction.js
@@ -42,7 +42,6 @@ export class UserAction extends plugin {
 
     //#我的纳戒
     async Show_najie(e) {
-        if (!verc({ e })) return false;
         let usr_qq = e.user_id.toString().replace('qg_', '');
         usr_qq = await Gulid(usr_qq);
         //有无存档
