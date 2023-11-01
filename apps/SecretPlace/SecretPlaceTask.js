1d12085823de519ee57991c1f6365b6e567e6f7c
xialuo03 <2531606029@qq.com>
2023-11-01 12:40:15 +0800

    优化秘境返回消息提示

--- apps/SecretPlace/SecretPlaceTask.js
+++ apps/SecretPlace/SecretPlaceTask.js
@@ -68,7 +68,7 @@ export class SecretPlaceTask extends plugin {
           }
         }
         //最后发送的消息
-        let msg = [];
+        let msg;
         //动作结束时间
         let end_time = action.end_time;
         //现在的时间
