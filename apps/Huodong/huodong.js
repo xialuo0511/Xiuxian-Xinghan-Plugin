f75343a3ee7780a6c5b44b47ecbf55c99d27af75
xialuo03 <2531606029@qq.com>
2024-02-05 14:11:05 +0800

    完成合成霄灯

--- apps/Huodong/huodong.js
+++ apps/Huodong/huodong.js
@@ -11,7 +11,7 @@ import { Gulid } from '../../api/api.js'
 
 
 
-export class DSC extends plugin {
+export class huodong extends plugin {
     constructor() {
         super({
             /** 功能名称 */
@@ -33,9 +33,72 @@ export class DSC extends plugin {
                 {
                     reg: '^#许愿.*$',
                     fnc: 'xuyuan'
+                },
+                {
+                    reg: '^#合成霄灯(*(.*))$',
+                    fnc: 'hecheng'
                 }
             ]
         })
+    }
+
+    async hecheng(e) {
+        //不开放私聊功能
+        if (!e.isGroup) {
+            e.reply('修仙游戏请在群聊中游玩');
+            return;
+        }
+        let usr_qq = e.user_id.toString().replace('qg_', '');
+        usr_qq = await Gulid(usr_qq);
+        //有无存档
+        let ifexistplay = await existplayer(usr_qq);
+        if (!ifexistplay) {
+            return;
+        }
+        let player = await Read_player(usr_qq);
+        let msg = e.msg.toString()
+        let shu = 1
+        if (msg.includes("*")) {
+            let code = msg.split("\*");
+            shu = code[1];
+            shu = await convert2integer(shu)
+        }
+        let shu1 = await exist_najie_thing(usr_qq, "浮空石", "材料");
+        let shu2 = await exist_najie_thing(usr_qq, "灵木", "材料");
+        let shu3 = await exist_najie_thing(usr_qq, "木浆纸", "材料");
+        if (shu1 < shu || shu2 < shu || shu3 < shu) {
+            e.reply(`材料不足，请查看纳戒相应材料数量后再合成！`)
+            return;
+        }
+        await Add_najie_thing(usr_qq, "浮空石", "材料", -shu);
+        await Add_najie_thing(usr_qq, "灵木", "材料", -shu);
+        await Add_najie_thing(usr_qq, "木浆纸", "材料", -shu);
+        await Add_najie_thing(usr_qq, "霄灯", "道具", shu);
+        e.reply(`合成成功，获得【霄灯】*${shu}`);
+        return;
+    }
+
+    async xuyuan(e) {
+        //不开放私聊功能
+        if (!e.isGroup) {
+            e.reply('修仙游戏请在群聊中游玩');
+            return;
+        }
+        let usr_qq = e.user_id.toString().replace('qg_', '')
+        usr_qq = await Gulid(usr_qq);
+        await Go(e);
+        let msg = e.msg.replace("#许愿", "");
+        //搜索纳戒物品
+        let shu = await exist_najie_thing(usr_qq, "霄灯", "道具");
+        if (!shu || shu < 1) {
+            e.reply("霄灯数量不足，请合成后再来!")
+            return;
+        }
+        await Add_najie_thing(usr_qq, "霄灯", "道具", -shu);
+        await Add_najie_thing(usr_qq, "愿力", "道具", shu);
+        await redis.set("xiuxian:yuanwang", `/n【${usr_qq}】${msg}`)
+        e.reply(`许愿成功！新的一年愿望一定会实现！`)
+        return;
     }
 
     async shop(e) {
@@ -44,6 +107,7 @@ export class DSC extends plugin {
             e.reply('修仙游戏请在群聊中游玩');
             return;
         }
+        await Go(e);
         let img = await get_huodongshop_img(e);
         e.reply(img);
         return;
@@ -126,4 +190,46 @@ export async function get_huodongshop_img(e) {
         ...data1,
     });
     return img;
+}
+
+export async function Go(e) {
+    let usr_qq = e.user_id.toString().replace('qg_', '')
+    usr_qq = await Gulid(usr_qq);
+    //不开放私聊
+    if (!e.isGroup) {
+        return;
+    }
+    //有无存档
+    let ifexistplay = await existplayer(usr_qq);
+    if (!ifexistplay) {
+        return;
+    }
+    //获取游戏状态
+    let game_action = await redis.get("xiuxian:player:" + usr_qq + ":game_action");
+    //防止继续其他娱乐行为
+    if (game_action == 0) {
+        e.reply("修仙：游戏进行中...");
+        return;
+    }
+    //查询redis中的人物动作
+    let action = await redis.get("xiuxian:player:" + usr_qq + ":action");
+    action = JSON.parse(action);
+    if (action != null) {
+        //人物有动作查询动作结束时间
+        let action_end_time = action.end_time;
+        let now_time = new Date().getTime();
+        if (now_time <= action_end_time) {
+            let m = parseInt((action_end_time - now_time) / 1000 / 60);
+            let s = parseInt(((action_end_time - now_time) - m * 60 * 1000) / 1000);
+            e.reply("正在" + action.action + "中,剩余时间:" + m + "分" + s + "秒");
+            return;
+        }
+    }
+    let player = await Read_player(usr_qq);
+    if (player.当前血量 < 200) {
+        e.reply("你都伤成这样了,先恢复一下再来吧！");
+        return;
+    }
+    allaction = true;
+    return;
 }
