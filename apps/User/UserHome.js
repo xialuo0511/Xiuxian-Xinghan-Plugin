0e7abe31881abbe9d5ac6ea2944571b021caf0f7
xialuo03 <2531606029@qq.com>
2024-03-02 21:03:13 +0800

    调试

--- apps/User/UserHome.js
+++ apps/User/UserHome.js
@@ -1208,18 +1208,11 @@ export class UserHome extends plugin {
             }
             //这里要找到丹药
             let this_danyao;
-            try {
-                this_danyao = data.danyao_list.find(item => item.name == thing_name)
-                    || data.newdanyao_list.find(item => item.name == thing_name);
-                try {
-                    if (this_danyao == undefined) {
-                        this_danyao = data.timedanyao_list.find(item => item.name == thing_name);
-                    }
-                } catch {
-                    this_danyao = data.timedanyao_list.find(item => item.name == thing_name);
-                }
-            } catch {
+            this_danyao = data.danyao_list.find(item => item.name == thing_name)
+            if (!this_danyao) {
                 this_danyao = data.timedanyao_list.find(item => item.name == thing_name);
+            } else if (!this_danyao) {
+                this_danyao = data.newdanyao_list.find(item => item.name == thing_name);
             }
             if ((this_danyao.type == "幸运" || this_danyao.type == "补天" || this_danyao.type == "补根") && quantity > 1) {
                 e.reply("说明书上写了：本丹药一次仅能服用一枚！");
@@ -1268,7 +1261,7 @@ export class UserHome extends plugin {
                 player.islucky = 10;
                 player.addluckyNo = this_danyao.xingyun;
                 player.幸运 += this_danyao.xingyun;
-                await data.setData("player", usr_qq, player);
+                data.setData("player", usr_qq, player);
                 e.reply(`${thing_name}服用成功，将在之后的 10 次冒险旅途中为你提高幸运值！`);
                 return;
             }
