6e9f63fb723bd4431fb8f4ad86c1c4fa3361a190
xialuo03 <2531606029@qq.com>
2024-03-03 13:32:49 +0800

    适配炼神丹

--- apps/Occupation/Occupation.js
+++ apps/Occupation/Occupation.js
@@ -740,21 +740,22 @@ export class Occupation extends plugin {
         //仙缘丹药
         let action2 = await redis.get('xiuxian:player:' + usr_qq + ':xianyuan');
         action2 = await JSON.parse(action2);
+        //炼神丹药
+        let action3 = await redis.get('xiuxian:player:' + usr_qq + ':xianyuan');
+        action3 = await JSON.parse(action3);
         let m = '丹药效果:';
         if (action2 && action2.ped > 0) {
             m += `\n仙缘丹药力${action2.xianyuangl * 100}%药效剩余${action2.ped}次`;
         }
-        // if (action.lianti > 0) {
-        //     m += `\n炼神丹药力${action.beiyong4 * 100}%药效${action.lianti
-        //         }次`;
-        // }
+        if (action3 && action3.lianti > 0) {
+            m += `\n炼神丹药力${action3.lianshen * 100}%药效${action3.lianti}次`;
+        }
         // if (action.beiyong2 > 0) {
         //     m += `\n神赐丹药力${action.beiyong3 * 100}% 药效${action.beiyong2
         //         }次`;
         // }
         if (action1 && action1.biguan > 0) {
-            m += `\n辟谷丹药力${action1.biguanxl * 100}%药效剩余${action1.biguan
-                }次`;
+            m += `\n辟谷丹药力${action1.biguanxl * 100}%药效剩余${action1.biguan}次`;
         }
         let player = await data.getData('player', usr_qq);
         if (player.islucky > 0) {
