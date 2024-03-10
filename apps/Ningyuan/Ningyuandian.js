df941c29a81818a0fc312f7c9166df31bbd6a8cd
xialuo03 <2531606029@qq.com>
2024-03-10 13:58:52 +0800

    测试

--- apps/Ningyuan/Ningyuandian.js
+++ apps/Ningyuan/Ningyuandian.js
@@ -142,8 +142,8 @@ export async function xh_zd(A_player, B_player) {
     }
 
     let msg = [];
-    msg.push(A_player)
-    msg.push(B_player)
+    // msg.push(A_player)
+    // msg.push(B_player)
     while (A_player.血量上限 > 0 && B_player.血量上限 > 0) {
         if (cut == 30) {
             msg.push("30回合未战胜魔物，挑战失败！")
