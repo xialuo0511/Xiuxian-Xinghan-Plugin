dff2e19be81f452cfe100cb919af565872897966
root <root@localhost>
2024-03-10 22:55:12 +0800

    调试

--- apps/User/BackUp.js
+++ apps/User/BackUp.js
@@ -318,7 +318,7 @@ export class BackUp extends plugin {
         }
 
       const timeStr = getTimeStr(backUpList[saveDataNum - 1]);
-      return e.reply(`数据库存档已读取：${timeStr}`);
+      return e.reply(`数据库存档已读取`);
     } catch (err) {
       await e.reply(`读取失败，${err}`);
       throw err;
