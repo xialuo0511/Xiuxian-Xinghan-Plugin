b0734a3ea6c8f959b6b087d7f5e36d6df72712e8
root <root@localhost>
2024-03-15 19:55:35 +0800

    2024-03-15 19:54:22

--- apps/Occupation/Occupation.js
+++ apps/Occupation/Occupation.js
@@ -570,7 +570,7 @@ export class Occupation extends plugin {
     async plant_jiesuan(user_id, time, is_random, group_id) {
 
         let usr_qq = user_id;
-        let player = data.getData("player", usr_id);
+        let player = data.getData("player", usr_qq);
         let now_level_id;
 
         if (!isNotNull(player.level_id)) {
@@ -579,8 +579,8 @@ export class Occupation extends plugin {
         let msg = []
         let a = {
         "type": "at",
-            "data": {
-                "qq": usr_id
+        "data": {
+            "qq": usr_qq
             }
         }
         msg.push(a);
