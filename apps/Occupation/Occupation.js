d44d31363a54fe0ebda6587ca3927a1f41b9360e
root <root@localhost>
2024-03-15 19:45:11 +0800

    尝试修复问题

--- apps/Occupation/Occupation.js
+++ apps/Occupation/Occupation.js
@@ -576,7 +576,13 @@ export class Occupation extends plugin {
         if (!isNotNull(player.level_id)) {
             return;
         }
-        let msg = ["at",usr_qq];
+        let msg = []
+        let a = {
+        "type": "at",
+            "data": {
+                "qq": usr_qq
+            }
+        }];
         // var size = this.xiuxianConfigData.plant.size;
         //let plant_amount1 = Math.floor((0.07+Math.random()*0.04)*time);
         //let plant_amount2 = Math.floor((0.07+Math.random()*0.04)*time);
