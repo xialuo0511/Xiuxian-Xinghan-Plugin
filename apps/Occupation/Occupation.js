e43ab41ffe85fd99be1ca165f7d5833da038b6ba
root <root@localhost>
2024-03-15 22:01:40 +0800

    2024-03-15 22:00:38

--- apps/Occupation/Occupation.js
+++ apps/Occupation/Occupation.js
@@ -576,7 +576,7 @@ export class Occupation extends plugin {
         if (!isNotNull(player.level_id)) {
             return;
         }
-        let msg = ["【"+player.名号+"】"]
+        let msg = [segment.at(user_id)]
         // var size = this.xiuxianConfigData.plant.size;
         //let plant_amount1 = Math.floor((0.07+Math.random()*0.04)*time);
         //let plant_amount2 = Math.floor((0.07+Math.random()*0.04)*time);
