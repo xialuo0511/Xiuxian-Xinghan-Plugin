1c59c01a16a6ef18e31e0288aedf3bcf7abce803
root <root@localhost>
2024-03-15 19:37:25 +0800

    尝试修复问题

--- apps/Occupation/Occupation.js
+++ apps/Occupation/Occupation.js
@@ -576,7 +576,7 @@ export class Occupation extends plugin {
         if (!isNotNull(player.level_id)) {
             return;
         }
-        let msg = [segment.at(usr_qq)];
+        let msg = ["at",usr_qq];
         // var size = this.xiuxianConfigData.plant.size;
         //let plant_amount1 = Math.floor((0.07+Math.random()*0.04)*time);
         //let plant_amount2 = Math.floor((0.07+Math.random()*0.04)*time);
--- apps/Occupation/OccupationTask.js
+++ apps/Occupation/OccupationTask.js
@@ -58,7 +58,7 @@ export class OccupationTask extends plugin {
                     }
                 }
                 //最后发送的消息
-                let msg = [segment.at(Number(player_id))];
+                let msg = ["at",Number(player_id)];
                 //动作结束时间
                 let end_time = action.end_time;
                 //现在的时间
