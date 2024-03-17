453172db0b12469710357c66acdff21f79cbe22d
xialuo03 <2531606029@qq.com>
2024-03-17 13:04:53 +0800

    修复问题

--- apps/SecretPlace/SecretPlaceTask.js
+++ apps/SecretPlace/SecretPlaceTask.js
@@ -78,7 +78,8 @@ export class SecretPlaceTask extends plugin {
         let player = await Read_player(player_id);
         //有秘境状态:这个直接结算即可
         if (action.Place_action == '0') {
-          // end_time = end_time;
+          //这里改一改,要在结束时间的前两分钟提前结算
+          end_time = end_time - action.time;
           //时间过了
           if (now_time > end_time) {
             let weizhi = action.Place_address;
