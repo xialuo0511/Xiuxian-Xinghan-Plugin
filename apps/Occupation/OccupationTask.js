44ca4d3378fdb29e85fb6434b1eecc6ea88b8615
xialuo03 <2531606029@qq.com>
2024-03-23 23:24:36 +0800

    修复问题

--- apps/Occupation/OccupationTask.js
+++ apps/Occupation/OccupationTask.js
@@ -156,12 +156,13 @@ export class OccupationTask extends plugin {
                         //msg.push(`\n采药归来，${ext}收获人参×${plant_amount1}，何首乌×${plant_amount2}，当归×${plant_amount3}，枸杞×${plant_amount4}`);
 
                         const sql2 = `DELETE FROM action WHERE id IN ${player_action.usr_id};`
-                        db1.query(sql2)
-                        if (is_group) {
-                            this.pushInfo(push_address, is_group, msg)
-                        } else {
-                            this.pushInfo(player_action.usr_id, is_group, msg);
-                        }
+                        db1.query(sql2, (err, result) => {
+                            if (is_group) {
+                                this.pushInfo(push_address, is_group, msg)
+                            } else {
+                                this.pushInfo(player_action.usr_id, is_group, msg);
+                            }
+                        })
 
                     }
                 }
