ee9f3bdf5ccf81740cca7d06e4558f51e6543cef
xialuo03 <2531606029@qq.com>
2024-03-24 16:17:06 +0800

    测试

--- apps/Ningyuan/Ningyuandian.js
+++ apps/Ningyuan/Ningyuandian.js
@@ -101,7 +101,7 @@ export class Ningyuandian extends plugin {
             let usr_qq = e.user_id;
             usr_qq = await Gulid(usr_qq)
             let player = await Read_player(usr_qq)
-            let sql1 = `select * from ningyuandian where this_level_time=${this.ningyuandianConfigData.Ningyuandian.level},usr_id=${usr_qq};`
+            let sql1 = `select * from ningyuandian where this_level_time=${this.ningyuandianConfigData.Ningyuandian.level} and usr_id=${usr_qq};`
             db.query(sql1, async (err, result) => {
 
                 let a = JSON.stringify(result)
@@ -143,7 +143,7 @@ export class Ningyuandian extends plugin {
                         }
                     }
                     await Add_najie_thing(usr_qq, "鎏金碎币", "道具", bi)
-                    let sql = `update ningyuandian set this_level='${b.this_level + 1}',level_${b.this_level + 1}_round=${zd_json.round},last_challenged_time=${now_time} where this_level_time=${this.ningyuandianConfigData.Ningyuandian.level},usr_id=${usr_qq};`
+                    let sql = `update ningyuandian set this_level='${b.this_level + 1}',level_${b.this_level + 1}_round=${zd_json.round},last_challenged_time=${now_time} where this_level_time=${this.ningyuandianConfigData.Ningyuandian.level} and usr_id=${usr_qq};`
                     db.query(sql, (err, result) => {
                         e.reply(`恭喜挑战成功，获得鎏金碎币*${bi}，进入下一层--第${b.this_level + 1}层!`)
                     })
