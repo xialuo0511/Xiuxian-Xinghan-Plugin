c9de6a985ee7f49601c0358d6d8bc78d9c9ad81f
xialuo03 <2531606029@qq.com>
2024-03-24 10:15:47 +0800

    修复问题

--- apps/Occupation/OccupationTask.js
+++ apps/Occupation/OccupationTask.js
@@ -257,22 +257,21 @@ export class OccupationTask extends plugin {
                         Add_职业经验(player_action.usr_id, exp);
                         msg.push(`\n狩猎归来，${ext}\n收获野兔×${end_amount}\n野鸡×${end_amount}\n野猪×${end_amount}\n野牛×${end_amount}\n野羊×${end_amount}\n`);
 
-                        const sql2 = `delete from action where usr_id=${player_action.usr_id};`
-                        db1.query(sql2, (err, result) => {
-                            if (is_group) {
-                                this.pushInfo(push_address, is_group, msg)
-                            } else {
-                                this.pushInfo(player_action.usr_id, is_group, msg);
-                            }
-                            db1.end();
-                        })
+
 
                     }
                 }
             }
-
+            const sql2 = `delete from action where usr_id=${player_action.usr_id};`
+            db1.query(sql2, (err, result) => {
+                if (is_group) {
+                    this.pushInfo(push_address, is_group, msg)
+                } else {
+                    this.pushInfo(player_action.usr_id, is_group, msg);
+                }
+                db1.end();
+            })
         })
-
     }
 
 
