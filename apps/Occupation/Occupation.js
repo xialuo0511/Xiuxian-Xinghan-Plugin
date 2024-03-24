116ec3a4e1f1f97e4da86e4217ba207e022a6b36
xialuo03 <2531606029@qq.com>
2024-03-24 10:41:16 +0800

    修复问题

--- apps/Occupation/Occupation.js
+++ apps/Occupation/Occupation.js
@@ -313,8 +313,10 @@ export class Occupation extends plugin {
         let sql1 = `select * from action where usr_id=${usr_qq};`
         db.query(sql1, (err, result) => {
             if (result) {
-                let action = JSON.stringify(result)
-                action = JSON.parse(action)
+                console.log(result)
+                let action0 = JSON.stringify(result)
+                action = JSON.parse(action0)
+                console.log(action)
                 let now_time = new Date().getTime();
                 let m = parseInt((action.end_time - now_time) / 1000 / 60);
                 let s = parseInt(((action.end_time - now_time) - m * 60 * 1000) / 1000);
