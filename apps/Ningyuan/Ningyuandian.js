5679ae6f84713890944a75dbfdae2974da45ac57
xialuo03 <2531606029@qq.com>
2024-03-24 12:25:26 +0800

    修复问题

--- apps/Ningyuan/Ningyuandian.js
+++ apps/Ningyuan/Ningyuandian.js
@@ -77,7 +77,7 @@ export class Ningyuandian extends plugin {
         }
         let sql1 = `select * from ningyuandian where usr_id=${usr_qq};`
         db.query(sql1, (err, result) => {
-            if (result) {
+            if (result != []) {
                 e.reply("您已报名！")
                 return;
             }
