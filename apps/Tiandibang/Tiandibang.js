204610943ab4a6f2fa2b7d4a67a26639fc5c2336
xialuo03 <2531606029@qq.com>
2024-03-26 22:48:48 +0800

    修复问题

--- apps/Tiandibang/Tiandibang.js
+++ apps/Tiandibang/Tiandibang.js
@@ -402,7 +402,6 @@ export class Tiandibang extends plugin {
                     let msg = Data_battle.msg;
                     let A_win = `${A_player.名号}击败了${B_player.名号}`;
                     let B_win = `${B_player.名号}击败了${A_player.名号}`;
-                    console.log(msg)
                     if (msg.find(item => item == A_win)) {
                         if (k == -1) {
                             tiandibang[x].jifen += 1500;
@@ -474,6 +473,7 @@ export class Tiandibang extends plugin {
                     let msg = Data_battle.msg;
                     let A_win = `${A_player.名号}击败了${B_player.名号}`;
                     let B_win = `${B_player.名号}击败了${A_player.名号}`;
+                    console.log(msg)
                     if (msg.find(item => item == A_win)) {
                         tiandibang[x].jifen += 1500;
                         tiandibang[x].cishu -= 1;
