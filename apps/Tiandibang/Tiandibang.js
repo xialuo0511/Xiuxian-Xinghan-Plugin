2a81adc3f534dded19f3a3d58ea8abaa0a2a6ade
xialuo03 <2531606029@qq.com>
2024-03-26 18:25:03 +0800

    修复问题

--- apps/Tiandibang/Tiandibang.js
+++ apps/Tiandibang/Tiandibang.js
@@ -9,7 +9,8 @@ import { ForwardMsg, Read_player, shijianc, Add_灵石, existplayer, Add_najie_t
 import { zd_battle } from "../Battle/Battle.js"
 import config from "../../model/Config.js"
 
-var mysql = require('mysql');
+const require = createRequire(import.meta.url)
+import mysql from "mysql"
 let databaseConfigData = config.getConfig("database", "database");
 //创建连接
 const db = mysql.createPool({
