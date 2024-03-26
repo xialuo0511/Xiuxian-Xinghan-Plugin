ec1c2160072a5f556f714b4519066311b1751afa
xialuo03 <2531606029@qq.com>
2024-03-26 21:04:22 +0800

    测试

--- apps/Tiandibang/Tiandibang.js
+++ apps/Tiandibang/Tiandibang.js
@@ -169,7 +169,7 @@ export class Tiandibang extends plugin {
             var dataString = JSON.stringify(result);
             let a = JSON.parse(dataString)
             a = a[0]
-            if (!dataString) {
+            if (!a) {
                 e.reply("你已经参赛了!")
                 return;
             }
