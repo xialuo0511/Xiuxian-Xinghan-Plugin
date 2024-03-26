b0e8dd59834837838bd0390774af4e564ced5d6e
xialuo03 <2531606029@qq.com>
2024-03-26 21:28:33 +0800

    修复问题

--- apps/Tiandibang/Tiandibang.js
+++ apps/Tiandibang/Tiandibang.js
@@ -134,6 +134,7 @@ export class Tiandibang extends plugin {
         if (!ifexistplay) {
             return;
         }
+        let sql1 = `select * from tiandibang where usr_id=${usr_qq};`
         db.query(sql1, async (err, result) => {
             var dataString = JSON.stringify(result);
             let a = JSON.parse(dataString)
