5d069b0a3668ffb7acc210b5f2a7562583fd6b37
xialuo03 <2531606029@qq.com>
2024-03-27 22:04:40 +0800

    测试

--- apps/Ningyuan/Ningyuandian.js
+++ apps/Ningyuan/Ningyuandian.js
@@ -141,7 +141,7 @@ export class Ningyuandian extends plugin {
                             bi += 20
                         }
                     }
-                    await Add_najie_thing(usr_qq, "鎏金碎币", "道具", bi)
+                    //await Add_najie_thing(usr_qq, "鎏金碎币", "道具", bi)
                     let sql = `update ningyuandian set this_level='${b.this_level + 1}',level_${b.this_level + 1}_round=${zd_json.round},last_challenged_time=${now_time} where this_level_time=${this.ningyuandianConfigData.Ningyuandian.level} and usr_id=${usr_qq};`
                     db.query(sql, (err, result) => {
                         e.reply(`恭喜挑战成功，获得鎏金碎币*${bi}，进入下一层--第${b.this_level + 1}层!`)
