739aca56dc59effa76ad550c006f546ec9b54a5d
xialuo03 <2531606029@qq.com>
2024-03-30 21:17:49 +0800

    调试

--- apps/User/PlayerControlTask.js
+++ apps/User/PlayerControlTask.js
@@ -64,9 +64,9 @@ export class PlayerControlTask extends plugin {
                 let end_time = player_action.end_time;
                 //现在的时间
                 let now_time = new Date().getTime();
+                console.log(player_action)
                 //闭关状态
                 if (player_action.action_biguan == "1") {
-                    console.log(player_action)
                     if (now_time < end_time) {
                         return;
                     }
