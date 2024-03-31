4a1dc877955ebfc7349a60295643ca5b68234cd0
xialuo03 <2531606029@qq.com>
2024-03-31 11:38:52 +0800

    调试

--- apps/SecretPlace/SecretPlaceplusTask.js
+++ apps/SecretPlace/SecretPlaceplusTask.js
@@ -498,7 +498,7 @@ export class SecretPlaceplusTask extends plugin {
               }
             })
           } else {
-            const sql2 = `update fuzhi set action_chengmi=${action.action_chengmi - 1},end_time=${new Date().getTime()} where usr_id=${action.usr_id};`
+            const sql2 = `update action set action_chengmi=${action.action_chengmi - 1},end_time=${new Date().getTime()} where usr_id=${action.usr_id};`
             db.query(sql2, async (err, result) => {
               //先完结再结算
               await Add_血气(action.usr_id, qixue);
