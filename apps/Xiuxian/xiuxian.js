7daaca24910763fdc5d667ee0f750885fa689b8b
xialuo03 <2531606029@qq.com>
2024-03-31 13:12:08 +0800

    调试

--- apps/Xiuxian/xiuxian.js
+++ apps/Xiuxian/xiuxian.js
@@ -3061,7 +3061,7 @@ export async function ForwardMsg(e, data) {
     if (msgList.length == 1) {
         await e.reply(msgList[0].message);
     } else {
-        await e.reply(await common.makeForwardMsg(e, [data], `修仙`));
+        await e.reply(await common.makeForwardMsg(e, data, `修仙`));
     }
     return;
 }
