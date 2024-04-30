1e4b5a2ad0987eb30b460a540263ff064b37bdbc
xialuo03 <2531606029@qq.com>
2024-04-30 23:12:18 +0800

    调试

--- apps/Ningyuan/Ningyuandian.js
+++ apps/Ningyuan/Ningyuandian.js
@@ -86,7 +86,7 @@ export class Ningyuandian extends plugin {
             return;
         }
         await Add_najie_thing(usr_qq, thing, wuping.type, wuping.shu1)
-        let sql2 = `update baoyufang set number=${wuping.number--} where name=${thing} `
+        let sql2 = `update baoyufang set number=${wuping.number--} where name='${thing}' `
         await sql_run(sql2)
         e.reply("购买成功！欢迎下次光临！")
         return;
