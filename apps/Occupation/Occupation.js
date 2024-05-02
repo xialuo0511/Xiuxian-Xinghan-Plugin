64275d7a5ef18dc1461c5635e3f83ff93d32cf48
xialuo03 <2531606029@qq.com>
2024-05-02 14:06:21 +0800

    调试

--- apps/Occupation/Occupation.js
+++ apps/Occupation/Occupation.js
@@ -246,6 +246,7 @@ export class Occupation extends plugin {
             const sql2 = `update fuzhi set occupation='${player.occupation}',occupxp=${player.occupation_exp},occupation_level=${player.occupation_level} where usr_id=${usr_qq};`
             db.query(sql2, (err, result) => {
                 if (err) {
+                    console.log(err)
                     e.reply("出现错误，请联系管理员，错误码fuzhi_03")
                     return
                 }
