6446e887654128f4bfa2158947c4f3bc33bca745
xialuo03 <2531606029@qq.com>
2024-06-28 12:07:41 +0800

    修复部分问题

--- apps/User/UserStart.js
+++ apps/User/UserStart.js
@@ -432,7 +432,7 @@ export class UserStart extends plugin {
         db.query(sql1, async (err, result) => {
             console.log(err)
             let action1 = JSON.stringify(result)
-            if (action1 != undefined || action1 != "undefined" || !action1) {
+            if (action1 != undefined && action1 != "undefined" && action1.isNotNull()) {
                 console.log(action1)
                 action1 = JSON.parse(action1)
                 action1 = action1[0]
