da8e8259c5ca336a770561501681b8d06e35eddc
xialuo03 <2531606029@qq.com>
2024-07-03 17:33:41 +0800

    调试

--- apps/Help/TopList.js
+++ apps/Help/TopList.js
@@ -208,8 +208,13 @@ export class TopList extends plugin {
             let player = await Read_player(this_qq);
             let sum_exp = await Get_xiuwei(this_qq);
             //境界名字需要查找境界名
-            console.log(data.Level_list.find(item => item.level_id == player.level_id))
-            let level = data.Level_list.find(item => item.level_id == player.level_id).level;
+            try {
+                let level = data.Level_list.find(item => item.level_id == player.level_id).level;
+            } catch (error) {
+                console.log(this_qq)
+                console.log(player)
+            }
+
             temp[i] = {
                 总修为: sum_exp,
                 境界: level,
