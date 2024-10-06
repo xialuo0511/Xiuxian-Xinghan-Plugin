caada4685ae8760fc435c9ebe8ad6f28a82c15a3
xialuo03 <2531606029@qq.com>
2024-10-06 19:10:24 +0800

    调试

--- apps/Xiuxian/xiuxian.js
+++ apps/Xiuxian/xiuxian.js
@@ -820,60 +820,66 @@ export async function exist_yijie_beibao_thing(usr_qq, thing_name, thing_class) 
  * 判断物品
  */
 export async function exist_najie_thing(usr_qq, thing_name, thing_class, thing_pinji = null) {
-    let najie = await Read_najie(usr_qq);
-    if (!isNotNull(najie.草药)) {
-        console.log("canshu:" + usr_qq + " " + thing_name + " " + thing_class)
-        najie.草药 = [];
-        await Write_najie(usr_qq, najie);
-    }
-    if (!isNotNull(najie.食材)) {
-        najie.食材 = [];
-        await Write_najie(usr_qq, najie);
-    }
-    if (!isNotNull(najie.盒子)) {
-        najie.盒子 = [];
-        await Write_najie(usr_qq, najie);
-    }
-    let ifexist;
-    if (thing_class == "装备") {
-        if (thing_pinji == null) {
-            ifexist = najie.装备.find(item => item.name == thing_name);
+    try {
+        let najie = await Read_najie(usr_qq);
+        if (!isNotNull(najie.草药)) {
+
+            najie.草药 = [];
+            await Write_najie(usr_qq, najie);
         }
-        else {
-            ifexist = najie.装备.find(item => item.name == thing_name && item.pinji == thing_pinji);
+        if (!isNotNull(najie.食材)) {
+            najie.食材 = [];
+            await Write_najie(usr_qq, najie);
         }
+        if (!isNotNull(najie.盒子)) {
+            najie.盒子 = [];
+            await Write_najie(usr_qq, najie);
+        }
+        let ifexist;
+        if (thing_class == "装备") {
+            if (thing_pinji == null) {
+                ifexist = najie.装备.find(item => item.name == thing_name);
+            }
+            else {
+                ifexist = najie.装备.find(item => item.name == thing_name && item.pinji == thing_pinji);
+            }
+        }
+        if (thing_class == "丹药") {
+            ifexist = najie.丹药.find(item => item.name == thing_name);
+        }
+        if (thing_class == "道具") {
+            ifexist = najie.道具.find(item => item.name == thing_name);
+        }
+        if (thing_class == "功法") {
+            ifexist = najie.功法.find(item => item.name == thing_name);
+        }
+        if (thing_class == "草药") {
+            ifexist = najie.草药.find(item => item.name == thing_name);
+        }
+        if (thing_class == "材料") {
+            ifexist = najie.材料.find(item => item.name == thing_name);
+        }
+        if (thing_class == "食材") {
+            ifexist = najie.食材.find(item => item.name == thing_name);
+        }
+        if (thing_class == "盒子") {
+            ifexist = najie.盒子.find(item => item.name == thing_name);
+        }
+        if (thing_class == "仙宠") {
+            ifexist = najie.仙宠.find(item => item.name == thing_name);
+        }
+        if (thing_class == "仙米") {
+            ifexist = najie.仙宠口粮.find(item => item.name == thing_name);
+        }
+        if (ifexist) {
+            return ifexist.数量;
+        }
+        return false;
+    } catch (error) {
+        console.log("异常:" + usr_qq + " " + thing_name + " " + thing_class)
+        return;
     }
-    if (thing_class == "丹药") {
-        ifexist = najie.丹药.find(item => item.name == thing_name);
-    }
-    if (thing_class == "道具") {
-        ifexist = najie.道具.find(item => item.name == thing_name);
-    }
-    if (thing_class == "功法") {
-        ifexist = najie.功法.find(item => item.name == thing_name);
-    }
-    if (thing_class == "草药") {
-        ifexist = najie.草药.find(item => item.name == thing_name);
-    }
-    if (thing_class == "材料") {
-        ifexist = najie.材料.find(item => item.name == thing_name);
-    }
-    if (thing_class == "食材") {
-        ifexist = najie.食材.find(item => item.name == thing_name);
-    }
-    if (thing_class == "盒子") {
-        ifexist = najie.盒子.find(item => item.name == thing_name);
-    }
-    if (thing_class == "仙宠") {
-        ifexist = najie.仙宠.find(item => item.name == thing_name);
-    }
-    if (thing_class == "仙米") {
-        ifexist = najie.仙宠口粮.find(item => item.name == thing_name);
-    }
-    if (ifexist) {
-        return ifexist.数量;
-    }
-    return false;
+
 }
 /**
  * 
