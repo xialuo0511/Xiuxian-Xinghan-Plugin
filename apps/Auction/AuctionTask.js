276c81654804938083f6697f8ad891d17f19bfd7
xialuo03 <2531606029@qq.com>
2024-10-06 21:36:19 +0800

    调试

--- apps/Auction/AuctionTask.js
+++ apps/Auction/AuctionTask.js
@@ -30,59 +30,64 @@ export class AuctionTask extends plugin {
   }
 
   async Auctiontask() {
-    let auction = await redis.get('xiuxian:AuctionofficialTask');
-    const redisGlKey = 'xiuxian:AuctionofficialTask_GroupList';
-    const groupList = await redis.sMembers(redisGlKey);
-    console.log(groupList)
-    if (!isNotNull(auction) || !groupList) {
-      return;
-    }
-    auction = JSON.parse(auction);
-    let nowTime = new Date().getTime();
-    let msg = '';
-    let last_offer_price = auction.last_offer_price;
-    if (auction.last_offer_price + 5 * 60 * 1000 > nowTime) {
-      let m = parseInt(
-        (last_offer_price + 5 * 60 * 1000 - nowTime) / 1000 / 60
-      );
-      let s = parseInt(
-        (last_offer_price + 5 * 60 * 1000 - nowTime - m * 60 * 1000) / 1000
-      );
-      msg = `${auction.thing.name}拍卖中，距离拍卖结束还有${m}分${s}秒，目前最高价${auction.last_price}`;
-      for (let group_id of groupList) {
-        this.pushInfo(group_id, true, msg);
+    try {
+      let auction = await redis.get('xiuxian:AuctionofficialTask');
+      const redisGlKey = 'xiuxian:AuctionofficialTask_GroupList';
+      const groupList = await redis.sMembers(redisGlKey);
+      console.log(groupList)
+      if (!isNotNull(auction) || !groupList) {
+        return;
       }
-    } else {
-      let last_offer_player = auction.last_offer_player;
-      if (last_offer_player == 0) {
-        // await Add_najie_thing(
-        //   auction.qq,
-        //   auction.thing.name,
-        //   auction.thing.class,
-        //   auction.amount,
-        //   auction.thing.pinji
-        // );
-        let player = await Read_player(auction.qq);
-        msg = `流拍，${auction.thing.name}已退回${player.名号}的纳戒`;
-      } else {
-        await Add_灵石(last_offer_player, -auction.last_price);
-        await Add_najie_thing(
-          last_offer_player,
-          auction.thing.name,
-          auction.thing.class,
-          auction.amount,
-          auction.thing.pinji
+      auction = JSON.parse(auction);
+      let nowTime = new Date().getTime();
+      let msg = '';
+      let last_offer_price = auction.last_offer_price;
+      if (auction.last_offer_price + 5 * 60 * 1000 > nowTime) {
+        let m = parseInt(
+          (last_offer_price + 5 * 60 * 1000 - nowTime) / 1000 / 60
         );
-        await Add_灵石(auction.qq, parseInt(auction.last_price * 0.9));
-        let player = await Read_player(last_offer_player);
-        msg = `拍卖结束，${player.名号}最终拍得该物品！`;
-      }
+        let s = parseInt(
+          (last_offer_price + 5 * 60 * 1000 - nowTime - m * 60 * 1000) / 1000
+        );
+        msg = `${auction.thing.name}拍卖中，距离拍卖结束还有${m}分${s}秒，目前最高价${auction.last_price}`;
+        for (let group_id of groupList) {
+          this.pushInfo(group_id, true, msg);
+        }
+      } else {
+        let last_offer_player = auction.last_offer_player;
+        if (last_offer_player == 0) {
+          // await Add_najie_thing(
+          //   auction.qq,
+          //   auction.thing.name,
+          //   auction.thing.class,
+          //   auction.amount,
+          //   auction.thing.pinji
+          // );
+          let player = await Read_player(auction.qq);
+          msg = `流拍，${auction.thing.name}已退回${player.名号}的纳戒`;
+        } else {
+          await Add_灵石(last_offer_player, -auction.last_price);
+          await Add_najie_thing(
+            last_offer_player,
+            auction.thing.name,
+            auction.thing.class,
+            auction.amount,
+            auction.thing.pinji
+          );
+          await Add_灵石(auction.qq, parseInt(auction.last_price * 0.9));
+          let player = await Read_player(last_offer_player);
+          msg = `拍卖结束，${player.名号}最终拍得该物品！`;
+        }
 
-      for (let group_id of groupList) {
-        this.pushInfo(group_id, true, msg);
+        for (let group_id of groupList) {
+          this.pushInfo(group_id, true, msg);
+        }
+        await redis.del('xiuxian:auction');
       }
-      await redis.del('xiuxian:auction');
+    } catch (error) {
+      console.log("AuctionTask异常： " + error);
     }
+
   }
 
   /**
