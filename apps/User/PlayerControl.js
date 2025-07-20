ad49005710d7099d1c21b5844fd60b9843466943
xialuo03 <2531606029@qq.com>
2025-07-20 19:27:27 +0800

    修复变量错误

--- apps/User/PlayerControl.js
+++ apps/User/PlayerControl.js
@@ -82,8 +82,8 @@ export class PlayerControl extends plugin {
 
     const currentAction = await DAL.getPlayerAction(usr_qq);
     if (currentAction) {
-      let m = Math.floor((currentAction.end_time - Date.now()) / 60000);
-      let s = Math.floor(((currentAction.end_time - Date.now()) % 60000) / 1000);
+      let m = Math.floor((currentAction.endTime - Date.now()) / 60000);
+      let s = Math.floor(((currentAction.endTime - Date.now()) % 60000) / 1000);
       e.reply(`你正在${currentAction.action}中，剩余时间: ${m}分${s}秒`);
       return;
     }
