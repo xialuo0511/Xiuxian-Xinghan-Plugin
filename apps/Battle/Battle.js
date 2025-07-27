452fb980662bb668cb979b319f3cd835918bfc0b
xialuo03 <2531606029@qq.com>
2025-07-27 22:56:42 +0800

    优化战斗木桩显示

--- apps/Battle/Battle.js
+++ apps/Battle/Battle.js
@@ -302,7 +302,7 @@ export class Battle extends plugin {
     const battleResult = await battleEngine({ ...A_data.player, id: userId, equipment: A_data.equipment }, dummy);
 
     // 只显示前10回合的战报
-    battleResult.log = battleResult.log.length > 21 ? battleResult.log.filter((_, index) => index < 21) : battleResult.log;
+    // battleResult.log = battleResult.log.length > 21 ? battleResult.log.filter((_, index) => index < 21) : battleResult.log;
     battleResult.log.push('\n...一顿操作后，木桩依旧屹立不倒...');
     let img = await this.renderBattle(e, battleResult);
     e.reply(img);
