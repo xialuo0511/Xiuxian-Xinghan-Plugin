ad28b465f01831f241346cd1b3b47ce00d4bbb5e
xialuo03 <2531606029@qq.com>
2025-07-28 21:58:50 +0800

    修复部分导入问题

--- logic/realm_logic.js
+++ logic/realm_logic.js
@@ -1,5 +1,4 @@
 import * as DAL from '../api/data-access.js';
-import { redisClient as redis } from '../api/redis.js';
 import { scheduleTask } from '../api/task-scheduler.js';
 import * as Notifier from '../handlers/notifier.js';
 import { battleEngine } from '../apps/Battle/Battle.js';
