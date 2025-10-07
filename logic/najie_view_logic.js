e9622f7396c876257edf9b17eb58e115e1ff43d3
xialuo03 <2531606029@qq.com>
2025-10-07 18:00:38 +0800

    移除调试内容

--- logic/najie_view_logic.js
+++ logic/najie_view_logic.js
@@ -146,23 +146,6 @@ export async function prepareNajieRenderData(userId, options = {}) {
     }
   }
 
-
-  console.log({
-    status: 'success',
-    renderData: {
-      pifu: player.练气皮肤,
-      user_id: userId,
-      player: player,
-      najie: najie,
-      paginatedItems: paginatedData.items,
-      pagination: paginatedData.pagination,
-      strand_hp: Strand(player.当前血量, player.血量上限),
-      strand_lingshi: Strand(najie.灵石, najie.灵石上限),
-      修仙版本: versionData,
-      colorMap: colorMap
-    }
-  });
-
   return {
     status: 'success',
     renderData: {
