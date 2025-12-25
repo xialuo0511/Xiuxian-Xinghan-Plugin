668d052de807064df5d5ad6a810f41e04fd8c76a
xialuo03 <2531606029@qq.com>
2025-12-25 10:02:01 +0800

    fix: 取消所有物品出售折扣，改为按出售价全额卖出

--- logic/shop_logic.js
+++ logic/shop_logic.js
@@ -143,14 +143,8 @@ export async function sellItem(userId, itemName, quantity = 1) {
              return { success: false, message: `[${itemName}]不可出售` };
         }
         
-        let sellPrice;
-        // 特殊处理：钞票类物品原价回收
-        if (itemInfo.type === '钞票') {
-            sellPrice = basePrice;
-        } else {
-            // 其他物品出售价格为原价的一半
-            sellPrice = Math.floor(basePrice * 0.5);
-        }
+        // 所有物品均按出售价全额卖出，不打折
+        let sellPrice = basePrice;
 
         if (sellPrice <= 0) {
              return { success: false, message: `[${itemName}]太廉价了，卖不出去` };
